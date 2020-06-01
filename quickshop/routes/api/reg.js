64db996d1c1aaf72de4482c2a2a93be1986568af
Joe Santos <jaysantos012@gmail.com>
2020-06-01 00:27:58 -0400

    creating the register routes login

--- quickshop/routes/api/reg.js
+++ quickshop/routes/api/reg.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('reg route');
-});
+
+
+router.post(
+    '/',
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more charaters'
+        ).isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+
+        res.send('reg route');
+    }
+);
 
 module.exports = router;
