29f850475fcd6385bc8a1e34bd19c883e794c516
Joe Santos <jaysantos012@gmail.com>
2020-05-31 23:02:12 -0400

    adding routes

--- quickshop/model/Users.js
+++ quickshop/model/Users.js
@@ -8,12 +8,16 @@ const UserSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     },
+    avatar: {
+        type: String,
+    
     date: {
         type: Date,
         default: Date.now
--- quickshop/server.js
+++ quickshop/server.js
@@ -7,6 +7,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/reg', require('./routes/api/reg'))
+app.use('/api/home', require('./routes/api/home'))
+app.use('/api/post', require('./routes/api/post'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
