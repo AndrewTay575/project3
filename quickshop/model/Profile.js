26f2707a11671d2f3a81417c0d2e03fad1759fc4
Joe Santos <jaysantos012@gmail.com>
2020-06-01 22:28:21 -0400

    adding auth routes,req, and adding middleware

---
+++ quickshop/model/Profile.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+         ref: 'user'
+    },
+
+    location: {
+        type: String
+      },
+      social: {
+        youtube: {
+          type: String
+        },
+        twitter: {
+          type: String
+        },
+        facebook: {
+          type: String
+        },
+        linkedin: {
+          type: String
+        },
+        instagram: {
+          type: String
+        }
+      },
+    bio: {
+        type: String
+      },
+    date: {
+        type: Date,
+        default: Date.now
+      }
+
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- quickshop/server.js
+++ quickshop/server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/reg', require('./routes/api/reg'))
 app.use('/api/home', require('./routes/api/home'))
-app.use('/api/post', require('./routes/api/post'))
+
 
 const PORT = process.env.PORT || 5000;
 
