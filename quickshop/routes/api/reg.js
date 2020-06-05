4e62d08d13e1c2feda30337cbabf7efb2346dc2f
Joe Santos <jaysantos012@gmail.com>
2020-06-04 23:01:17 -0400

    finished the routes for the profile page, and creating and profile and updating a profile

--- quickshop/routes/api/reg.js
+++ quickshop/routes/api/reg.js
@@ -5,13 +5,14 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
 const User = require('../../model/Users');
 
 router.post(
     '/',
     [
-        check('name', 'Name is required').not().isEmpty(),
+        check('username', 'Username is required').not().isEmpty(),
         check('email', 'Please include a valid email').isEmail(),
         check(
             'password',
@@ -24,7 +25,7 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { name, email, password } = req.body;
+        const { username, email, password } = req.body;
 
         try {
             let user = await User.findOne({ email });
@@ -45,7 +46,7 @@ router.post(
             );
 
             user = new User({
-                name,
+                username,
                 email,
                 avatar,
                 password
@@ -60,7 +61,7 @@ router.post(
                 user: {
                   id: user.id
                 }
-            }
+            };
 
             jwt.sign(
                 payload,
